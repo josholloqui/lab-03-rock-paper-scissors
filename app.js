5630fd16215c35f4164a6fb1f8ed8349b1e9be10
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-07-08 16:20:57 -0700

    this commits adds html, css, and button in js

---
+++ app.js
@@ -0,0 +1,6 @@
+// import functions and grab DOM elements
+const playButton = document.querySelector('#battle');
+console.log(playButton);
+// initialize state
+
+// set event listeners to update state and DOM
