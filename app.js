2db6396e52eab5db1367ecd1d099829869d05694
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-07-09 16:36:53 -0700

    refactor and add js function tests

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 import { getRandomThrow, checkResult } from './get.random.throw.js';
-// grabs button
+
+// GET DOM elements
 const playButton = document.getElementById('battle');
 const resetButton = document.getElementById('reset');
-// following code grabs spans
+
 const battleWins = document.getElementById('wins');
 const battleDraws = document.getElementById('draws');
 const battleLosses = document.getElementById('losses');
-// following reveals results section
+
 const resultsReveal = document.getElementById('results-reveal');
 const winReveal = document.getElementById('win-reveal');
 const drawReveal = document.getElementById('draw-reveal');
 const lossReveal = document.getElementById('loss-reveal');
 const hideRules1 = document.getElementById('game-rules-1');
 
-
-// initialize state
+// initialize global state
 let wins = 0;
 let draws = 0;
 let losses = 0;
-// set event listeners to update state and DOM
 
 playButton.addEventListener('click', () => {
-    // grabs radio button(image)
     const radioCheck = document.querySelector('input:checked');
-    // grabs user battle element
+
+    // Grabs battle elements
     const userThrow = radioCheck.value;
-    // grabs computer battle element
     const computerThrow = getRandomThrow();
+
     // time to reveal the winner
     switch (checkResult(userThrow, computerThrow)) {
         case 0:
@@ -62,5 +61,5 @@ playButton.addEventListener('click', () => {
 });
 
 resetButton.addEventListener('click', () => {
-    return window.location.reload(true);
+    return window.location.reload();
 });
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,78 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { getRandomThrow, checkResult } from '../get.random.throw.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('This will test if function randomThrow returns random string', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 'string';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = typeof(getRandomThrow());
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('This will test if function checkResult returns a result using RPS logic for ROCK', (expect) => {
+    //Arrange
+    // Expect the function to return correct values
+    const expected0 = 0;
+    const expected1 = 1;
+    const expected2 = 2;
+    
+    //Act 
+    // Calls the function to see if the expected values are correct
+    const actual0 = checkResult('rock', 'rock');
+    const actual1 = checkResult('rock', 'scissors');
+    const actual2 = checkResult('rock', 'paper');
+
+    //Expect
+    // Checks if actual and expected are equal
+    expect.equal(actual0, expected0);
+    expect.equal(actual1, expected1);
+    expect.equal(actual2, expected2);
+});
+
+test('This will test if function checkResult returns a result using RPS logic for PAPER', (expect) => {
+    //Arrange
+    // Expect the function to return correct values
+    const expected0 = 0;
+    const expected1 = 1;
+    const expected2 = 2;
+    
+    //Act 
+    // Calls the function to see if the expected values are correct
+    const actual0 = checkResult('paper', 'paper');
+    const actual1 = checkResult('paper', 'rock');
+    const actual2 = checkResult('paper', 'scissors');
+
+    //Expect
+    // Checks if actual and expected are equal
+    expect.equal(actual0, expected0);
+    expect.equal(actual1, expected1);
+    expect.equal(actual2, expected2);
+});
+
+test('This will test if function checkResult returns a result using RPS logic for SCISSORS', (expect) => {
+    //Arrange
+    // Expect the function to return correct values
+    const expected0 = 0;
+    const expected1 = 1;
+    const expected2 = 2;
+    
+    //Act 
+    // Calls the function to see if the expected values are correct
+    const actual0 = checkResult('scissors', 'scissors');
+    const actual1 = checkResult('scissors', 'paper');
+    const actual2 = checkResult('scissors', 'rock');
+
+    //Expect
+    // Checks if actual and expected are equal
+    expect.equal(actual0, expected0);
+    expect.equal(actual1, expected1);
+    expect.equal(actual2, expected2);
 });
